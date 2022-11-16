90c452666b8dcdcdbe27d87d73ba5381941a6b61
matelko <mateusz280798@gmail.com>
2022-11-16 21:28:22 +0100

    Add User model

---
+++ WebAPITemplate.Shared/Models/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPITemplate.Shared.Models.Entities;
+
+public class User
+{
+    [Key] public Guid Id { get; set; } = Guid.NewGuid();
+    [Required]
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public bool IsEnabled { get; set; }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
