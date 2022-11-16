9079bc4c475e31d582bc96e4a03fd661467773fb
matelko <mateusz280798@gmail.com>
2022-11-16 21:33:23 +0100

    Add DbContext

---
+++ WebAPITemplate.API/Data/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPITemplate.API.Data;
+
+public class AppDbContextFactory
+{
+    private readonly DbContextOptions<AppDbContext> _options;
+
+    public AppDbContextFactory(DbContextOptions<AppDbContext> options)
+    {
+        _options = options;
+    }
+
+    public AppDbContext Create() => new AppDbContext(_options);
+}
--- WebAPITemplate.API/Program.cs
+++ WebAPITemplate.API/Program.cs
@@ -1,37 +1,53 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.EntityFrameworkCore;
+using WebAPITemplate.API.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-    .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllers();
+    
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+        .AddNegotiate();
+
+    builder.Services.AddAuthorization(options =>
+    {
+        // By default, all incoming requests will be authorized according to the default policy.
+        options.FallbackPolicy = options.DefaultPolicy;
+    });
+    
+    builder.Services.AddDbContext<AppDbContext>(
+        options => options
+            .UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking),
+        ServiceLifetime.Singleton);
+        
+    builder.Services.AddDbContextFactory<AppDbContext>(
+        options => options
+            .UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking),
+        ServiceLifetime.Scoped);
 }
 
-app.UseHttpsRedirection();
+WebApplication app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthentication();
-app.UseAuthorization();
+    app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.Run();
+    app.MapControllers();
+
+    app.Run();
+}
